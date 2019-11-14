362aca77e191108a38bb23b7776b09a9f63b2150
Tomi Juden <tomi.juden@gmail.com>
2019-11-14 22:28:19 +0200

    Moved navigation to components

---
+++ src/components/BottomNav.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-bottom-navigation
+    background-color="blue accent-4"
+    color="white"
+    dark
+    fixed
+    grow
+    v-if="$auth.isAuthenticated"
+  >
+    <v-btn
+      @click="truncateList"
+    >
+      <span>Tyhjennä</span>
+      <v-icon>mdi-autorenew</v-icon>
+    </v-btn>
+
+    <v-btn
+      v-if="currentList==1"
+      disabled
+      @click="removeList"
+    >
+      <span>Poista</span>
+      <v-icon>mdi-delete</v-icon>
+    </v-btn>
+
+    <v-btn @click.stop="importModalVisible = true">
+      <span>Tuo lista</span>
+      <v-icon>mdi-download</v-icon>
+    </v-btn>
+
+    <v-btn
+      :disabled="imported===0"
+      @click="saveImported"
+    >
+      <span>Tallenna</span>
+      <v-icon>mdi-content-save</v-icon>
+    </v-btn>
+
+  </v-bottom-navigation>
+</template>
+
+<script>
+export default {
+  name: 'BottomNav',
+  props: {
+    currentList: {
+      type: Number,
+    },
+    imported: {
+      type: Number,
+    }
+  },
+  methods: {
+    truncateList() {
+      this.$emit('truncateList');
+    },
+    removeList() {
+      this.$emit('removeList');
+    },
+    saveImported() {
+      this.$emit('saveImported');
+    },
+    showImportModal() {
+      this.$emit('showImportModal');
+    }
+  }
+}
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,122 @@
+<template>
+  <div v-if="!$auth.loading">
+    <v-snackbar
+      v-model="snackbar"
+      :top="y === 'top'"
+      :color="color"
+    >
+      {{ text }}
+    </v-snackbar>
+    <v-app-bar
+      color="blue accent-4"
+      dense
+      dark
+      v-if="$auth.isAuthenticated"
+    >
+      <v-btn
+        icon
+        @click="logout"
+      >
+        <v-icon>mdi-export-variant</v-icon>
+      </v-btn>
+
+      <v-btn icon @click="itemModalVisible">
+        <v-icon>mdi-plus-circle</v-icon>
+      </v-btn>
+
+      <v-btn icon @click="reload">
+        <v-icon>mdi-refresh</v-icon>
+      </v-btn>
+
+      <v-btn icon @click="listModalVisible">
+        <v-icon>mdi-folder-plus-outline</v-icon>
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-menu
+        left
+        bottom
+        >
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item
+            v-for="(elist, i) in eventlists"
+            :key="`${i}`"
+            @click="changeList(elist)"
+            >
+            <v-list-item-title v-text="elist.title"></v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+    <v-app-bar
+      color="blue accent-4"
+      dense
+      dark
+      v-if="!$auth.isAuthenticated"
+    >
+      <v-toolbar-title>Login</v-toolbar-title>
+
+      <v-btn
+        icon
+        @click="login"
+      >
+        <v-icon>mdi-login-variant</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  props: {
+    eventlists: {
+      type: Array,
+    },
+    y: {
+      type: String,
+    },
+    color: {
+      type: String,
+    },
+    text: {
+      type: String,
+    },
+    snackbar: {
+      type: Boolean,
+    }
+  },
+  methods: {
+    login() {
+      this.$emit('login');
+    },
+    logout() {
+      this.$emit('logout');
+    },
+    reload() {
+      this.$emit('reload');
+    },
+    showItemModal() {
+      this.$emit('showItemModal');
+    },
+    showListModal() {
+      this.$emit('showListModal');
+    },
+    changeList() {
+      this.$emit('changeList');
+    },
+    itemModalVisible() {
+      this.$emit('showItemModal');
+    },
+    listModalVisible() {
+      this.$emit('showListModal');
+    }
+  }
+}
+</script>
